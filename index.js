ba111465f1fb595593890bdd08f2ac68a6883d78
LordByron66 <lordbyronleon66@gmail.com>
2022-12-27 12:04:06 -0500

    cambio index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const app = require('./app');
+const app = require('../eventsClient/src/app');
 
 app.listen(app.get('port'));
 console.log('El servidor esta en el puerto', app.get('port'));
